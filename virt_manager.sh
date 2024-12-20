9733ac7e838f24d46adee2c8eff59d66b07c32da
Matvei Sologubov <matthew.sologubov@gmail.com>
2024-12-20 17:08:16 +0300

    feat: Fixed script for installing virtual manager

--- virt_manager.sh
+++ virt_manager.sh
@@ -4,11 +4,6 @@ sudo systemctl start libvirtd
 
 sudo systemctl enable libvirtd
 
-
-sudo sh -c "echo "unix_sock_group = \"libvirt\"" >> /etc/libvirt/libvirtd.conf"
-sudo sh -c "echo "unix_sock_ro_perms = \"0777\"" >> /etc/libvirt/libvirtd.conf"
-sudo sh -c "echo "unix_sock_rw_perms = \"0770\"" >> /etc/libvirt/libvirtd.conf"
-
-
-
-
+sudo echo "unix_sock_group = \"libvirt\"" >> /etc/libvirt/libvirtd.conf
+sudo echo "unix_sock_ro_perms = \"0777\"" >> /etc/libvirt/libvirtd.conf
+sudo echo "unix_sock_rw_perms = \"0770\"" >> /etc/libvirt/libvirtd.conf
